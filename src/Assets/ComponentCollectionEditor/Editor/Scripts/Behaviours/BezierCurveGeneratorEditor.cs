1919cbc9052fcc305acbb8a127bc1c8f78a9b4ed
Michael Kolb <efil.threat@googlemail.com>
2021-07-02 22:26:28 +0200

    added custom editor basic set up

--- src/Assets/ComponentCollectionEditor/Editor/Scripts/Behaviours/BezierCurveGeneratorEditor.cs
+++ src/Assets/ComponentCollectionEditor/Editor/Scripts/Behaviours/BezierCurveGeneratorEditor.cs
@@ -12,5 +12,38 @@ public class BezierCurveGeneratorEditor : Editor
 
     #region Methods
 
+    #region OnInspectorGUI
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        BezierCurveGenerator bezierCurveGenerator = this.target as BezierCurveGenerator;
+
+        switch (bezierCurveGenerator.Type)
+        {
+            case Assets.ComponentCollection.Scripts.Enums.BezierTypes.Linear:
+                // show two points
+                break;
+            case Assets.ComponentCollection.Scripts.Enums.BezierTypes.Quadratic:
+                // show two points and one pivot
+                break;
+            case Assets.ComponentCollection.Scripts.Enums.BezierTypes.Cubic:
+                // show two points and two pivots
+                break;
+            default:
+                break;
+        }
+        /*
+        myScript.flag = GUILayout.Toggle(myScript.flag, "Flag");
+
+        if (myScript.flag)
+        {
+            myScript.i = EditorGUILayout.IntSlider("I field:", myScript.i, 1, 100);
+        }
+        */
+    }
+    #endregion OnInspectorGUI
+
     #endregion Methods
 }
